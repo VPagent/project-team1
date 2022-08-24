6b4fde5bf2001233fcbe6f92d5c5e945c72774b1
VPagent <vpagent13@gmail.com>
2022-08-24 14:39:19 +0300

    bagfixHeader

--- src/js/header/header_value.js
+++ src/js/header/header_value.js
@@ -6,7 +6,6 @@ import { pagination } from '../pagination/pagination';
 const errorText = document.querySelector('.error-text');
 const searchBtn = form[1];
 const CURRENT_FILMS_KEY = 'current films';
-
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
@@ -14,25 +13,22 @@ function onFormSubmit(event) {
   errorText.classList.remove('is-visible');
 
   const inputValue = event.target[0].value.trim('');
-  filmGallery.innerHTML = '';
 
   fetchMovieSearch(inputValue).then(data => {
 
     if (!inputValue || data.results.length === 0) {
       errorText.classList.add('is-visible');
       searchBtn.classList.add('is-hidden');
-      filmGallery.innerHTML = markError();
+
       removeClass();
-      return;
+      return
     }
-
+    filmGallery.innerHTML = '';
     filmGallery.insertAdjacentHTML('beforeend', renderMarkup(data));
-    filmGallery.insertAdjacentHTML(
-      'beforeend',
-      pagination(data.page, data.total_pages, inputValue)
-    );
-    localStorage.setItem(CURRENT_FILMS_KEY, JSON.stringify(data.results));
+    
+    pagination(data.page, data.total_pages, inputValue)
 
+    localStorage.setItem(CURRENT_FILMS_KEY, JSON.stringify(data.results));
   });
   form.reset();
 }
@@ -43,11 +39,6 @@ function removeClass() {
     searchBtn.classList.remove('is-hidden');
   }, 2000);
 }
-function markError() {
-  return `<div class="error-box">
-          <h2 class="error-box__whoops">Whoops(</h2>
-          <p class="error-box__text">Search result not successful. Enter the correct movie name and</p>
-    </div>`;
-}
+
 
 export { onFormSubmit, form };
