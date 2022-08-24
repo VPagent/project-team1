388861e7ae0771f08894317a20d90d07c404cd89
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-08-24 09:06:06 +0300

    add fitch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { form, onFormSubmit, addLocalStore } from './js/header/header_value';
 import { renderMarkup } from './js/templates/renderMarkup';
-import './js/footer/footer-modal'
+import './js/footer/footer-modal';
 import { pagination } from './js/pagination/pagination';
 import {
   getTrendData,
@@ -9,7 +9,7 @@ import {
   getGenre,
 } from './js/api/api_fetch';
 import { renderMarkupModal } from './js/mainModal/renderMarkupModal';
-import { openToggleModal } from './js/mainModal/mainModal';
+import './js/mainModal/mainModal';
 
 export const filmGallery = document.querySelector('.film-gallery__list');
 export const GENRES_KEY = 'genres';
@@ -27,6 +27,6 @@ getTrendData()
   .then(response => {
     filmGallery.insertAdjacentHTML('beforeend', renderMarkup(response));
     pagination(response.page, response.total_pages);
-    addLocalStore(response)
+    addLocalStore(response);
   })
   .catch(console.error);
