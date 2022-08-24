b7d22eb167957442204325bb77dae7f8c65d4370
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-08-24 15:37:06 +0300

    01

--- src/js/footer/footer-modal.js
+++ src/js/footer/footer-modal.js
@@ -1,21 +1,35 @@
+
+import showConfetti from './confetti';
+
 const openPopUp = document.querySelector('.open-pop-up');
-console.log(openPopUp);
 const closePopUp = document.querySelector('.pop-up-close');
 const popUp = document.querySelector('.pop-up');
 const body = document.querySelector('body');
+const popUpContainer = document.querySelector('.pop-up-container');
+
 
 openPopUp.addEventListener('click', function (e) {
   e.preventDefault();
   popUp.classList.add('open');
-  // body.classList.add(modal_open);
+  showConfetti();
+  body.classList.add('disable-scroll');
 });
 
 closePopUp.addEventListener('click', () => {
   popUp.classList.remove('open');
+  body.classList.remove('disable-scroll');
 });
 
 body.addEventListener('keyup', e => {
   if (e.code === 'Escape') {
     popUp.classList.remove('open');
+    body.classList.remove('disable-scroll');
   }
 });
+
+window.addEventListener('click', (e) => {
+  if (e.target == popUpContainer) {
+    popUp.classList.remove('open');
+    body.classList.remove('disable-scroll');
+  }
+})
