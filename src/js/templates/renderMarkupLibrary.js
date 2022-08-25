06b6e53f7d2991950dcc9e69ba9387999c7bb5e6
he1gi <oleh.mishchenko1@gmail.com>
2022-08-25 18:59:56 +0300

    new request

--- src/js/templates/renderMarkupLibrary.js
+++ src/js/templates/renderMarkupLibrary.js
@@ -9,8 +9,33 @@ console.log(pagination);
 
 const savedFilms = localStorage.getItem('current films');
 const parsedFilms = JSON.parse(savedFilms);
-console.log(parsedFilms); // settings object
-// console.log(localStorage.setItem("genres"));
+
+myLibraryContainer.innerHTML = '';
+renderMarkupLibrary(parsedFilms);
+
+headerButtonlist.addEventListener('click', getFilmsKey);
+function getFilmsKey(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  if (evt.target.classList.contains('js-watched')) {
+    filmKey = 'Watched';
+    const savedFilms = localStorage.getItem(filmKey);
+    const parsedFilms = JSON.parse(savedFilms);
+    myLibraryContainer.innerHTML = '';
+    renderMarkupLibrary(parsedFilms);
+  }
+  if (evt.target.classList.contains('js-queue')) {
+    filmKey = 'Queue';
+    console.log(filmKey);
+    const savedFilms = localStorage.getItem(filmKey);
+    const parsedFilms = JSON.parse(savedFilms);
+    myLibraryContainer.innerHTML = '';
+    renderMarkupLibrary(parsedFilms);
+  }
+}
+
+// console.log(parsedFilms); // settings object
 
 export function renderMarkupLibrary(parsedFilms) {
   const markup = parsedFilms
@@ -54,14 +79,39 @@ export function renderMarkupLibrary(parsedFilms) {
       }
     )
     .join('');
-
+  myLibraryContainer.insertAdjacentHTML('beforeend', markup);
   let page = 10;
   const totalPages = Math.ceil(parsedFilms.length / 20);
   pagination(page, totalPages, paginationContainer, onClickEvt);
-  return markup;
+  // return markup;
 }
 
-myLibraryContainer.insertAdjacentHTML(
-  'beforeend',
-  renderMarkupLibrary(parsedFilms)
-);
+// myLibraryContainer.insertAdjacentHTML(
+//   'beforeend',
+//   renderMarkupLibrary(parsedFilms)
+// );
+
+//         <div class="film-gallery__image-wrapper">
+//         <img class="film-gallery__image" src="${POSTER_IMG_URL}"
+//         alt="${title || name}" width ="394" height ="336"/>
+//         </div>
+//         <div class="film-gallery__info-wrapper">
+//         <h3 class="film-gallery__name">${name || title}</h3>
+//         <p class="film-gallery__info">
+//         <span class="film-gallery__info-genres">
+//         ${matchGenresById(genre_ids) || 'No info'}</span>
+//         |
+//         <span class="film-gallery__info-year">${
+//           release_date ? release_date.slice(0, 4) : 'No info'
+//         }</span>
+//                       <span class="film-gallery__info-rang">${
+//                         vote_average || 'No info'
+//                       }</span>
+//                       </p>
+//                       </div>
+//                       </li>`;
+//       }
+//     )
+//     .join('');
+
+// }
